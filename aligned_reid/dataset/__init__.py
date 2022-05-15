2177efe9654a27b8bfa15405d8aff7fbb3dad9a0
Kefilino Khalifa F <kefilino@live.com>
2022-05-15 16:25:38 +0700

    add QuerySet initialization

--- aligned_reid/dataset/__init__.py
+++ aligned_reid/dataset/__init__.py
@@ -1,4 +1,5 @@
 import numpy as np
+import os
 import os.path as osp
 ospj = osp.join
 ospeu = osp.expanduser
@@ -7,6 +8,7 @@ from ..utils.utils import load_pickle
 from ..utils.dataset_utils import parse_im_name
 from .TrainSet import TrainSet
 from .TestSet import TestSet
+from .QuerySet import QuerySet
 
 
 def create_dataset(
@@ -16,7 +18,7 @@ def create_dataset(
   assert name in ['market1501', 'cuhk03', 'duke', 'combined'], \
     "Unsupported Dataset {}".format(name)
 
-  assert part in ['trainval', 'train', 'val', 'test'], \
+  assert part in ['trainval', 'train', 'val', 'test', 'query'], \
     "Unsupported Dataset Part {}".format(part)
 
   ########################################
@@ -24,8 +26,8 @@ def create_dataset(
   ########################################
 
   if name == 'market1501':
-    im_dir = ospeu('~/Dataset/market1501/images')
-    partition_file = ospeu('~/Dataset/market1501/partitions.pkl')
+    im_dir = ospeu('./dataset/market1501/images')
+    partition_file = ospeu('./dataset/market1501/partitions.pkl')
 
   elif name == 'cuhk03':
     im_type = ['detected', 'labeled'][0]
@@ -52,7 +54,8 @@ def create_dataset(
                     first_match_break=True)
 
   partitions = load_pickle(partition_file)
-  im_names = partitions['{}_im_names'.format(part)]
+  if not part == 'query':
+    im_names = partitions['{}_im_names'.format(part)]
 
   if part == 'trainval':
     ids2labels = partitions['trainval_ids2labels']
@@ -92,6 +95,31 @@ def create_dataset(
       marks=marks,
       **kwargs)
 
+  elif part == 'query':
+    marks = partitions['test_marks']
+    kwargs.update(cmc_kwargs)
+    gal_dir = input('Enter gallery directory: ')
+    if not osp.isdir(gal_dir):
+      print('Invalid directory.')
+      return
+    im_names = [f for f in os.listdir(gal_dir) if osp.isfile(osp.join(gal_dir, f))]
+    if len(im_names) == 0:
+      print('Directory is empty.')
+      return
+    query_im = input('Enter query path: ')
+    if query_im == '':
+      print('Invalid path.')
+      return
+    elif not osp.exists('{}'.format(query_im)):
+      print('File \'{}\' not found.'.format(query_im))
+      return
+      
+    ret_set = QuerySet(
+      gal_dir=gal_dir,
+      gal_names=im_names,
+      query_im=query_im,
+      **kwargs)
+
   if part in ['trainval', 'train']:
     num_ids = len(ids2labels)
   elif part in ['val', 'test']:
@@ -105,10 +133,10 @@ def create_dataset(
   print(('-' * 40))
   print(('{} {} set'.format(name, part)))
   print(('-' * 40))
-  print(('NO. Images: {}'.format(len(im_names))))
-  print(('NO. IDs: {}'.format(num_ids)))
 
   try:
+    print(('NO. Images: {}'.format(len(im_names))))
+    print(('NO. IDs: {}'.format(num_ids)))
     print(('NO. Query Images: {}'.format(num_query)))
     print(('NO. Gallery Images: {}'.format(num_gallery)))
     print(('NO. Multi-query Images: {}'.format(num_multi_query)))
